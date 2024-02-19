05e2c3892aa31dddda426bc78a794ee78344c25f
Brett Gorden <brettgorden@Bretts-MBP-2.lan>
2024-02-18 23:35:12 -0800

    Added null safe operators to blade components

--- resources/views/components/job-type.blade.php
+++ resources/views/components/job-type.blade.php
@@ -2,7 +2,7 @@
     @if (!empty($tabindex)) tabindex="{{ $tabindex }}" @endif {{ !empty($required) ? 'required' : '' }}>
     <option>Select Job Type</option>
     @foreach (App\Enums\JobType::cases() as $case)
-        <option value="{{ $case->value }}"{{ $selected->value === $case->value ? ' selected' : '' }}>
+        <option value="{{ $case->value }}"{{ $selected?->value === $case->value ? ' selected' : '' }}>
             {{ $case->value }}
         </option>
     @endforeach

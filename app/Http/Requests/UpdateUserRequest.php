19c05568c3982032b3980fbcc02646a0fd7ffea0
Brett Gorden <brettgorden@Bretts-MBP-2.lan>
2024-02-18 22:10:36 -0800

    Secure requests with Auth::check()

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rules\Password;
 
 /**
@@ -17,7 +18,7 @@ class UpdateUserRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return Auth::check();
     }
 
     /**
